506f4e28a2ebf9cda140d786611fc074ea15b9d8
lena <evkhodosova@gmail.com>
2022-05-22 23:32:01 +0300

    change description

--- src/AboutMe/PersonalCard.tsx
+++ src/AboutMe/PersonalCard.tsx
@@ -6,15 +6,28 @@ function PersonalCard() {
         <div className='card'>
             <div>
                 <div className='cardHeader'>Hello! I'm Lena</div>
-                I am junior web developer based in St. Petersburg.
-                I love code, modern user-friendly interfaces and develop high-quality applications.
+                I am junior software engineer based in St. Petersburg.
+                I love code, develop high-quality applications and create something new.
                 <br/>
                 <br/>
                 I study Computer Science and Engineering at ITMO University
+                <br/>
+                <br/>
+                Always open to work on interesting projects,
+                <br/>
+                so feel free to contact me ʕ ᵔᴥᵔ ʔ
             </div>
             <div>
                 <div className='cardSkillsHeader'> My skills:</div>
-                <div> React & Redux, TypeScript, JavaScript, HTML & CSS, REST, Java, Git</div>
+                <br/>
+                <div><span className='cardSkillsName'>Languages: </span>
+                     Java, Kotlin, TypeScript, Python, C++, HTML & CSS</div>
+                <br/>
+                <div><span className='cardSkillsName'>Technologies: </span>
+                     Spring, Hibernate, Postgresql, Junit, REST, React</div>
+                <br/>
+                <div><span className='cardSkillsName'>Other: </span>
+                     Git, bash, Linux, Docker</div>
             </div>
         </div>
     )
